776675aadbf12d93382b407b18a7b7f873972891
QuilGrafit <quillgrafit@gmail.com>
2025-06-19 19:00:18 +0300

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -1,1 +1,21 @@
+import { connectToDatabase } from '../lib/db.js';
+import { HoroscopeGenerator } from '../lib/horoscope.js';
 
+export default async (req, res) => {
+  const db = await connectToDatabase();
+  const users = await db.collection('users').find({}).toArray();
+  
+  for (const user of users) {
+    try {
+      const horoscope = HoroscopeGenerator.generate(user);
+      await bot.telegram.sendMessage(user.id, horoscope.text, {
+        parse_mode: 'HTML',
+        reply_markup: horoscope.keyboard
+      });
+    } catch (error) {
+      console.error(`Error sending to ${user.id}:`, error);
+    }
+  }
+  
+  res.status(200).send('Cron job completed');
+};
